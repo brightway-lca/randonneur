608d5cbdf0a956e07e45b0b8c58ffac24eb720df
Chris Mutel <cmutel@gmail.com>
2024-07-23 09:30:16 +0200

    Update constants.py

--- randonneur/constants.py
+++ randonneur/constants.py
@@ -2,17 +2,26 @@ class MappingConstants:
     SIMAPRO_CSV = {
         "expression language": "like JSONPath",
         "labels": {
-            "identifier": "Process[*].\"Process identifier\".text",
+            "identifier": 'Process[*]."Process identifier".text',
             "name": "Process[*].Products[*].text[0]",
-            "platform_id": "Process[*].\"Platform Identifier\""
+            "platform_id": 'Process[*]."Platform Identifier"',
         },
     }
     ECOSPOLD2 = {
         "expression language": "XPath",
         "labels": {
+            "filename": "concat(//*:activity/@id, '_', //*:intermediateExchange[*:outputGroup = '0' and @amount > 0]/@intermediateExchangeId, '.spold')",
             "name": "//*:activityName/text()",
             "location": "//*:geography/*:shortname/text()",
             "reference product": "//*:intermediateExchange[*:outputGroup = '0' and @amount > 0]/*:name/text()",
-            "unit": "//*:intermediateExchange[*:outputGroup = '0' and @amount > 0]/*:unitName/text()"
-        }
+            "unit": "//*:intermediateExchange[*:outputGroup = '0' and @amount > 0]/*:unitName/text()",
+        },
+    }
+    ECOSPOLD2_BIO = {
+        "expression language": "XPath",
+        "labels": {
+            "name": "//*:elementaryExchange/*:name/text()",
+            "unit": "//*:elementaryExchange/*:unitName/text()",
+            "uuid": "//*:elementaryExchange/@elementaryExchangeId",
+        },
     }
